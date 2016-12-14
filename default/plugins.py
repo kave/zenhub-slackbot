79119b09446adbba397d9a8471716f2da18a2675
etapau <etapau@gmail.com>
2016-12-13 23:33:43 -0500

    basic setup for slackbot

---
+++ default/plugins.py
@@ -0,0 +1,25 @@
+from slackbot.bot import *
+import re
+
+@respond_to('hi', re.IGNORECASE)
+def hi(message):
+    message.reply('I can understand hi or HI!')
+    # react with thumb up emoji
+    message.react('+1')
+
+@respond_to('I love you')
+def love(message):
+    message.reply('I love you too!')
+
+@listen_to('Can someone help me?')
+def help(message):
+    # Message is replied to the sender (prefixed with @user)
+    message.reply('Yes, I can!')
+
+    # Message is sent on the channel
+    # message.send('I can help everybody!')
+
+
+@default_reply
+def my_default_hanlder(message):
+    message.reply('Sorry but I didn\'t understand you')
